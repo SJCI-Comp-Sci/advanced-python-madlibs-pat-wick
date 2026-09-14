422c78ce662aab569bed96b30c081048fb4aa587
Pat Wick <110044297+pat-wick@users.noreply.github.com>
2026-09-14 14:05:24 +0000

    added input functions for words

--- madlibs.py
+++ madlibs.py
@@ -1,13 +1,17 @@
 """
 Mad Libs
-Name:
-Date:
+Name: Mr. Wick
+Date: 9/14/26
 
 A program that asks the user for several words and inserts them into a story.
 """
 
 # Collect at least 4 words from the user with input()
-
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+number = input("Enter a number: ")
+noun2 = input("Enter a noun: ")
 
 # Build the story with an f-string
 
